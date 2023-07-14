67c5ef1d2684ad5fa56f7f31265ebd4ef7291321
北省 <jih6666@126.com>
2023-07-14 17:28:25 +0800

    添加文档

---
+++ .vuepress/.temp/pages/src/JavaSE/单元测试_日志.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-d164ac44\",\"path\":\"/src/JavaSE/%E5%8D%95%E5%85%83%E6%B5%8B%E8%AF%95_%E6%97%A5%E5%BF%97.html\",\"title\":\"单元测试$日志\",\"lang\":\"zh-CN\",\"frontmatter\":{\"description\":\"单元测试$日志 1.单元测试 1.1概述【理解】 JUnit是一个 Java 编程语言的单元测试工具。JUnit 是一个非常重要的测试工具 1.2特点【理解】 JUnit是一个开放源代码的测试工具。 提供注解来识别测试方法。 JUnit测试可以让你编写代码更快，并能提高质量。 JUnit优雅简洁。没那么复杂，花费时间较少。 JUnit在一个条中显示进度。如果运行良好则是绿色；如果运行失败，则变成红色。\"},\"headers\":[{\"level\":2,\"title\":\"1.单元测试\",\"slug\":\"_1-单元测试\",\"link\":\"#_1-单元测试\",\"children\":[{\"level\":3,\"title\":\"1.1概述【理解】\",\"slug\":\"_1-1概述【理解】\",\"link\":\"#_1-1概述【理解】\",\"children\":[]},{\"level\":3,\"title\":\"1.2特点【理解】\",\"slug\":\"_1-2特点【理解】\",\"link\":\"#_1-2特点【理解】\",\"children\":[]},{\"level\":3,\"title\":\"1.3使用步骤【应用】\",\"slug\":\"_1-3使用步骤【应用】\",\"link\":\"#_1-3使用步骤【应用】\",\"children\":[]},{\"level\":3,\"title\":\"1.4相关注解【应用】\",\"slug\":\"_1-4相关注解【应用】\",\"link\":\"#_1-4相关注解【应用】\",\"children\":[]}]},{\"level\":2,\"title\":\"2.日志\",\"slug\":\"_2-日志\",\"link\":\"#_2-日志\",\"children\":[{\"level\":3,\"title\":\"2.1概述【理解】\",\"slug\":\"_2-1概述【理解】\",\"link\":\"#_2-1概述【理解】\",\"children\":[]},{\"level\":3,\"title\":\"2.2日志体系结构和Log4J【理解】\",\"slug\":\"_2-2日志体系结构和log4j【理解】\",\"link\":\"#_2-2日志体系结构和log4j【理解】\",\"children\":[]},{\"level\":3,\"title\":\"2.3入门案例【应用】\",\"slug\":\"_2-3入门案例【应用】\",\"link\":\"#_2-3入门案例【应用】\",\"children\":[]},{\"level\":3,\"title\":\"2.4配置文件详解【理解】\",\"slug\":\"_2-4配置文件详解【理解】\",\"link\":\"#_2-4配置文件详解【理解】\",\"children\":[]},{\"level\":3,\"title\":\"2.5在项目中的应用【应用】\",\"slug\":\"_2-5在项目中的应用【应用】\",\"link\":\"#_2-5在项目中的应用【应用】\",\"children\":[]}]}],\"readingTime\":{\"minutes\":5.17,\"words\":1552},\"filePathRelative\":\"src/JavaSE/单元测试&日志.md\",\"excerpt\":\"<h1> 单元测试$日志</h1>\\n<h2> 1.单元测试</h2>\\n<h3> 1.1概述【理解】</h3>\\n<p>JUnit是一个 Java 编程语言的单元测试工具。JUnit 是一个非常重要的测试工具</p>\\n<h3> 1.2特点【理解】</h3>\\n<ul>\\n<li>JUnit是一个开放源代码的测试工具。</li>\\n<li>提供注解来识别测试方法。</li>\\n<li>JUnit测试可以让你编写代码更快，并能提高质量。</li>\\n<li>JUnit优雅简洁。没那么复杂，花费时间较少。</li>\\n<li>JUnit在一个条中显示进度。如果运行良好则是绿色；如果运行失败，则变成红色。</li>\\n</ul>\",\"autoDesc\":true}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
